73fa941eeba68f495d8e3d63b86d77845405122f
Leonardo Goes <leonardotaqua20@hotmail.com>
2026-01-07 00:32:51 -0300

    chore: initial commit

---
+++ carshop-api/src/main/java/com/carshop/carshop_api/CarshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.carshop.carshop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarshopApiApplication.class, args);
+	}
+
+}
---
+++ carshop-api/src/test/java/com/carshop/carshop_api/CarshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carshop.carshop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
